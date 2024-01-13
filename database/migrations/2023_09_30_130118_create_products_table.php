ed8fa7fec25d567595eb4fa830161248c34843cc
mrcrross <Crrrya38@yandex.ru>
2024-01-13 13:18:22 +0800

    unique

--- database/migrations/2023_09_30_130118_create_products_table.php
+++ database/migrations/2023_09_30_130118_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->integer('price');
             $table->string('image')->nullable();
             $table->unsignedBigInteger('type_id');
